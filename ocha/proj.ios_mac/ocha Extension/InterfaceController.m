7e724bd3de7fddc0ddb381b3ff97b2fb2ca16532
HidehikoKondo <kondo@wonderpla.net>
2016-05-22 02:51:43 +0900

    Siriさんしゃべるよ 加速度センサーの値を取得

--- ocha/proj.ios_mac/ocha Extension/InterfaceController.m
+++ ocha/proj.ios_mac/ocha Extension/InterfaceController.m
@@ -7,14 +7,19 @@
 //
 
 #import "InterfaceController.h"
+#import <CoreMotion/CoreMotion.h>
 
 @interface InterfaceController()
 @property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceLabel *messageLabel;
 
+float xPos;
+
 @end
 
 
-@implementation InterfaceController
+@implementation InterfaceController{
+    CMMotionManager *motionManager;
+}
 
 - (void)awakeWithContext:(id)context {
     [super awakeWithContext:context];
@@ -31,8 +36,41 @@
 
 - (void)willActivate {
     // This method is called when watch view controller is about to be visible to user
+
+    //加速度センサー開始
+    motionManager = [[CMMotionManager alloc] init];
+    [motionManager startAccelerometerUpdates];
+    [self setupAccelerometer];
+    
     [super willActivate];
 }
+
+
+- (void)setupAccelerometer{
+    if (motionManager.accelerometerAvailable){
+        // センサーの更新間隔の指定、２Hz
+        motionManager.accelerometerUpdateInterval = 0.2f;
+        
+        // ハンドラを設定
+        CMAccelerometerHandler handler = ^(CMAccelerometerData *data, NSError *error)
+        {
+            // 加速度センサー
+            double xac = data.acceleration.x;
+            double yac = data.acceleration.y;
+            double zac = data.acceleration.z;
+            
+            // 画面に表示
+            NSLog([NSString stringWithFormat:@"x: %0.3f", xac]);
+            //NSLog([NSString stringWithFormat:@"y: %0.3f", yac]);
+            //NSLog([NSString stringWithFormat:@"z: %0.3f", zac]);
+            
+        };
+        
+        // 加速度の取得開始
+        [motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue currentQueue] withHandler:handler];
+    }
+}
+
 
 - (void)didDeactivate {
     // This method is called when watch view controller is no longer visible
